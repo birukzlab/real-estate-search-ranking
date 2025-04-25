26b9655b5e3dae13f95729342feda13134d54f7b
birukzlab <birukalemu63@gmail.com>
2025-04-25 13:33:39 -0500

    Files and Folders Structure Ready

---
+++ data/fetch_kaggle_data.py
@@ -0,0 +1,20 @@
+import os
+import zipfile
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+def download_dataset(dataset_path, download_dir):
+    api=KaggleApi()
+    api.authenticate()
+
+    print(f"Downloading {dataset_path} to {download_dir}...")
+    api.dataset_download_files(dataset_path, path=download_dir, unzip=True)
+
+
+if __name__ == "__main__":
+    dataset_slug = "ahmedshahriarsakib/usa-real-estate-dataset"
+    download_path = "../data/raw"
+    os.makedirs(download_path, exist_ok=True)
+    download_dataset(dataset_slug, download_path)
+
+
