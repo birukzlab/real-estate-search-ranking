841186f25267bc2f6643d046aa93891265962687
birukzlab <birukalemu63@gmail.com>
2025-04-26 17:15:50 -0500

    EDA done

--- data/fetch_kaggle_data.py
+++ data/fetch_kaggle_data.py
@@ -13,7 +13,7 @@ def download_dataset(dataset_path, download_dir):
 
 if __name__ == "__main__":
     dataset_slug = "ahmedshahriarsakib/usa-real-estate-dataset"
-    download_path = "../data/raw"
+    download_path = "data/raw"
     os.makedirs(download_path, exist_ok=True)
     download_dataset(dataset_slug, download_path)
 
